1754babe8643969e9fbf60794002edcc25448e5f
Md Noman Hossain <mhossain222159@bscse.uiu.ac.bd>
2025-06-15 04:20:01 +0600

    Added pro1.py code

---
+++ Week1_Practice/pro1.py
@@ -0,0 +1,16 @@
+print(f"Enter the number: ", end="")
+n = input()
+n = int(n)
+
+def fibonacchi(n):
+    if(n==1):
+        return 0
+    elif(n==2):
+        return 1
+    else:
+        return fibonacchi(n-1)+fibonacchi(n-2)
+    
+result = fibonacchi(n)
+print(f'The {n}th term of the series is {result}')
+print(f'The 11th term of the series is {fibonacchi(11)}')
+
